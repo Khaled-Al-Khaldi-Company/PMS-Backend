4e19fa0e5bcff20ac30ce547fd422d2a65daa866
Bait Al-Watan Admin <admin@bait-al-watan.com>
2026-05-20 00:27:12 +0300

    chore: restore app.service greeting message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! v2-contract-qty-verification';
+    return 'Hello World!';
   }
 }
